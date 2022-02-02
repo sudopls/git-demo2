d79361dedcb987b737c6e28cd5eb605d5419367f
Fredrik <fl@kommandokringla.se>
2022-02-02 14:20:02 +0100

    Added first part of content

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ echo "hello world";
 
 echo "vue.js";
 
-echo "laravel"
+echo "laravel";
 
 echo "laravel-auth";
+
+echo "<main><h1>Hello heading</h1>";
+
+echo "<p>lorem ipsum dolor</p>";
+
+echo "</main>";
 ?>
